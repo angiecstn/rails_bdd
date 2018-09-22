c22170253af87cfbb88d493696d1eaa1f3986d2c
Albert Stjärne <albert.stjarne@gmail.com>
2018-09-22 16:52:04 +0200

    adds new articles page, title error handling

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,6 +15,8 @@ class ArticlesController < ApplicationController
       @article = Article.new(article_params)
       if @article.save
         redirect_to @article
+      else
+        render 'new'
       end
     end
   

23a9af5f1d060976c47f8f5753eb3488681a4fc6
Albert Stjärne <albert.stjarne@gmail.com>
2018-09-22 10:50:38 +0200

    adds feature for leave comment

---
+++ features/step_definitions/leave_a_comment_steps.rb
@@ -0,0 +1,19 @@
+Given("I am on the landing page") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("I click the link {string}") do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I am on the article page") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I fill in the {string} with {string}") do |string, string2|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I click on the link {string}") do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end

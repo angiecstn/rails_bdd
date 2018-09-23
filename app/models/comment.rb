1596427635cede9eb20b47da087b634e8ab6d018
Angelica Costantini <angelicacostantini07@gmail.com>
2018-09-23 15:25:02 +0200

    adds email validation if present

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,11 @@ class Comment < ApplicationRecord
   belongs_to :article
   validates :commenter, presence: true
   validates :body, presence: true
-  validates :email, presence: true 
+  validates_format_of  :email, :with => /\A[\+A-Z0-9\._%-]+@([A-Z0-9-]+\.)+[A-Z]{2,4}\z/i, unless: lambda { self.email.blank? }
+  # else 
+  #   render 'new comment'
+  # end
 end
+
+
+
